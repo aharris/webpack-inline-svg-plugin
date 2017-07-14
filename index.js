ca5622e79e4fcf0ad9a3e01be754c0cc5bc2bff6
Ashton Harris <ashton.harris@ey.com>
2017-07-14 14:58:51 -0400

    Add class and title to svg

--- index.js
+++ index.js
@@ -16,9 +16,14 @@ WebpackInlineSvgPlugin.prototype.apply = function (compiler) {
       if ( matches) {
         for (let i = 0; i < matches.length; i ++) {
           if (matches[i].indexOf( 'inline-svg' ) !== -1) {
+			console.log(matches[i])
+			let classes = matches[i].split('class=\\"')[1] ? matches[i].split('class=\\"')[1].split('\\"')[0] : '';
+			let title = matches[i].split('title=\\"')[1] ? matches[i].split('title=\\"')[1].split('\\"')[0] : '';
             let svgContents = fs.readFileSync( self.options.basePath + matches[i].split('src=\\"')[1].split('\\">')[0]);
 
-            module._source._value = module._source._value.replace(matches[i], svgContents.toString().replace(/\n|\t/g, ' ').replace(/"/g, '\\"').trim());
+			let newSvg = svgContents.toString().replace('<svg', `<svg class="${classes}" title="${title}"`);
+
+            module._source._value = module._source._value.replace(matches[i], newSvg.toString().replace(/\n|\t/g, ' ').replace(/"/g, '\\"').trim());
           }
         }
 

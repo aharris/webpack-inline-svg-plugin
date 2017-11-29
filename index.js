738722ab99b221b56f8ca20a419ed30642088ca3
Ashton Harris <ashton.harris@ey.com>
2017-11-29 12:18:40 -0500

    Fix attribute order

--- index.js
+++ index.js
@@ -18,14 +18,15 @@ WebpackInlineSvgPlugin.prototype.apply = function (compiler) {
           if (matches[i].indexOf( 'inline-svg' ) !== -1) {
 			let classes = matches[i].split('class=\\"')[1] ? matches[i].split('class=\\"')[1].split('\\"')[0] : '';
 			let title = matches[i].split('title=\\"')[1] ? matches[i].split('title=\\"')[1].split('\\"')[0] : '';
-            let svgContents = fs.readFileSync( self.options.basePath + matches[i].split('src=\\"')[1].split('\\">')[0]);
+            let svgContents = fs.readFileSync( self.options.basePath + matches[i].split('src=\\"')[1].split('\\"')[0]);
 
 			let newSvg = svgContents.toString().replace('<svg', `<svg class="${classes}" title="${title}"`);
 
-            module._source._value = module._source._value.replace(matches[i], newSvg.toString().replace(/\n|\t/g, ' ').replace(/"/g, '\\"').trim());
+            module._source._value = module._source._value.replace(matches[i], newSvg.toString()
+				.replace(/\n|\t/g, ' ')
+				.replace(/"/g, '\\"').trim());
           }
         }
-
       }
     }
   }
